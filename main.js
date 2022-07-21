973f65048da571e860cc1b31f79095ef3af74c15
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2022-07-20 23:39:05 -0400

    Primeia versão funcional

--- main.js
+++ main.js
@@ -1,11 +1,30 @@
 const peoples = require("./pessoas.json");
 
-let res = []
 
-for (people of peoples) {
+/**
+ * @param objectToFilter Array de objetos para ser filtrado
+ * @param attrsToSearch Array de atributos a serem pesquisados
+ * @param attrsValues Array com valores que vão servir de filtro
+ * @returns Array que contem os resultados do filtro
+ */
+function filterByAtrr(objectToFilter, attrsToSearch, attrsValues) {
     
-    if (people.nome == "Fabiana"){
-        res.push(people)
+    let filteredArray = []
+
+    for (let i = 0; i < objectToFilter.length; i++) {
+        for (let j = 0; j < attrsToSearch.length; j++) {
+            for (let k = 0; k < attrsValues.length; k++) {
+                
+                if (objectToFilter[i][attrsToSearch[j]] == attrsValues[k]) {
+                    filteredArray.push(objectToFilter[i])
+               }
+            }
+        }
     }
+
+   return filteredArray
 }
-console.log(res);
+
+
+let resp = filterByAtrr(peoples, ["nome"], ["Tereza Luzia Gomes"])
+console.log(resp);
